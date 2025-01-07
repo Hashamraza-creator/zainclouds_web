50a0636b993072a8aace4816c4312b7053d11711
Hasham Raza <138536709+Hashamraza-creator@users.noreply.github.com>
2025-01-08 03:04:07 +0500

    new interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { UpperNavComponent } from './upper-nav/upper-nav.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
-
+import { ServiceComponent } from './service/service.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,28 +12,11 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
     UpperNavComponent,
     NavbarComponent,
     HeroSectionComponent,
+    ServiceComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ZainClouds';
 
-  slides = [
-    {
-      url: 'https://via.placeholder.com/1920x1080?text=Mobile+Development',
-      title: 'Web Development',
-      description: 'Build scalable and responsive web applications',
-    },
-    {
-      url: 'https://via.placeholder.com/1920x1080?text=Mobile+Development',
-      title: 'Mobile Development',
-      description: 'Create seamless mobile app experiences',
-    },
-    {
-      url: 'https://via.placeholder.com/1920x1080?text=Cloud+Services',
-      title: 'AWS Cloud Services',
-      description: 'Leverage AWS for powerful cloud solutions',
-    },
-  ];
 }
--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, Input } from '@angular/core';
-import { title } from 'node:process';
+
 
 @Component({
   selector: 'app-hero-section',
@@ -9,34 +9,4 @@ import { title } from 'node:process';
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.css'],
 })
-export class HeroSectionComponent {
-@Input() slides: any[] = [];
-@Input() autoPlay = false;
-@Input() autoPlaySpeed=1000;
-currentSlide = 0;
-
-
-
-
-
-
-next(){
-  this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-}
-
-previous(){
-  this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length
-}
-goToSlide(index: number): void {
-  this.currentSlide = index;
-}
-
-constructor(){
-  if(this.autoPlay) {
-    setInterval(() => {
-      this.next();
-
-    },  this.autoPlaySpeed);
-  }
-}
-}
+export class HeroSectionComponent {}
